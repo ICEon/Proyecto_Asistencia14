0f560bf31adfbdd8c3ca020aca152c2c53c43b54
Eon Matrix <matrix_eon@yahoo.com>
2014-02-14 06:00:56 -0600

    Uno

---
+++ js/acciones.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+$(document).ready(function(){
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function onDeviceReady() {
+
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+$("li").tap(function() {
+	var Asiste = $(this).attr("data-icon");
+var $Quien = $(this).attr('id');
+	if (Asiste == 'delete')
+	{
+		$(this).buttonMarkup({ icon: "check" });
+		$('#' + $Quien + ' a').css('color', '#0F0');
+	}
+	
+	if (Asiste == 'check')
+	{
+  	    $(this).buttonMarkup({ icon: "delete" });
+		$('#' + $Quien + ' a').css('color', '#F00');
+	}
+
+    });  
+
+   }
+});
+
+            function gotFS(fileSystem) {
+   fileSystem.root.getDirectory("Datos_Asistencia", {create: true}, gotDir);
+}
+
+function gotDir(dirEntry) {
+    dirEntry.getFile("Asistencia.txt", {create: true, exclusive: true}, gotFileEntry);
+
+
+            }
+
+            function gotFileEntry(fileEntry) {
+                fileEntry.createWriter(gotFileWriter, fail);
+            }
+
+            function gotFileWriter(writer) {
+                writer.onwrite = function(evt) {
+                    console.log("Correcto");
+                };
+
+                writer.write("Esto es el contenido");
+                writer.abort();
+                // contents of file now 'some different text'
+            }
+
+            function fail(error) {
+                console.log("error : "+error.code);
+            }
